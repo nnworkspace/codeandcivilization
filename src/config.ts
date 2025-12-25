8341e24df431fbdbdf84782e54cc62e283a1e51d
Ning Zhao <nnworkspace@gmail.com>
2025-12-26 00:37:54 +0100

    fix config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,4 @@
-import type { Site, SocialObjects } from "./types";
-
-export const SITE: Site = {
+export const SITE = {
   website: "https://code-and-civilisation.vercel.app/", // Fixed URL (hyphens)
   author: "Ning Zhao",
   desc: "Disinterested contemplation on the architecture of work, the ascent of systems, and the art of building civilization.",
@@ -9,7 +7,11 @@ export const SITE: Site = {
   lightAndDarkMode: true,
   postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-};
+  showArchives: true,
+  dynamicOgImage: true,
+  dir: "ltr", // "rtl" | "auto"
+  timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/
+} as const;
 
 export const LOCALE = {
   lang: "en",
@@ -21,26 +23,4 @@ export const LOGO_IMAGE = {
   svg: true,
   width: 216,
   height: 46,
-};
-
-// Keep this array! The template needs it to build.
-export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/nnworkspace/codeandcivilization", // Change to your repo later
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/nizhao/", // Change to your profile later
-    linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:nnworkspace@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-];
+} as const;
