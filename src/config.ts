734ec5f7bd2ef740ff6138c1cb1c598636828c50
Ning Zhao <nnworkspace@gmail.com>
2025-12-26 00:48:59 +0100

    fix config.ts again

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
 export const SITE = {
   website: "https://code-and-civilisation.vercel.app/", // Fixed URL (hyphens)
   author: "Ning Zhao",
+  profile: "https://www.linkedin.com/in/nizhao/",
   desc: "Disinterested contemplation on the architecture of work, the ascent of systems, and the art of building civilization.",
   title: "Code and Civilisation",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
+  postPerIndex: 5,
   postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
@@ -16,6 +18,7 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
+  lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Europe/Berlin", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/
 } as const;
 
