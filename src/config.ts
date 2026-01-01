2aa26812ada1aa5e7c06d99ea395f51651307eba
Ning Zhao <nnworkspace@gmail.com>
2026-01-01 10:07:40 +0100

    update site config

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Code and Civilisation",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 5,
-  postPerPage: 5,
+  postPerIndex: 10,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
